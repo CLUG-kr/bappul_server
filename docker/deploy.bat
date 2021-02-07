b675fbf3774d85e925b2b0c184ef5c6be19b3b4c
JaeHoon Kim <choco730522@gmail.com>
2021-02-07 16:18:43 +0900

    admin 유저 전체 조회 기능 추가함

--- docker/deploy.bat
+++ docker/deploy.bat
@@ -1,3 +1,3 @@
 docker login --username AWS -p $(aws ecr get-login-password --region ap-northeast-2) 511624941057.dkr.ecr.ap-northeast-2.amazonaws.com
-docker build -t 511624941057.dkr.ecr.ap-northeast-2.amazonaws.com/testserver:0.0.1 . --no-cache
-docker push 511624941057.dkr.ecr.ap-northeast-2.amazonaws.com/testserver:0.0.1
+docker build -t 511624941057.dkr.ecr.ap-northeast-2.amazonaws.com/testserver:0.1.0 . --no-cache
+docker push 511624941057.dkr.ecr.ap-northeast-2.amazonaws.com/testserver:0.1.0
