94c9c47bee70dda165a518d914df07e1de93732d
JaeHoon Kim <choco730522@gmail.com>
2021-02-11 18:01:52 +0900

    ormconfig 서버 배포에 맞게 바꿈

--- docker/deploy.bat
+++ docker/deploy.bat
@@ -1,3 +1,3 @@
 docker login --username AWS -p $(aws ecr get-login-password --region ap-northeast-2) 511624941057.dkr.ecr.ap-northeast-2.amazonaws.com
-docker build -t 511624941057.dkr.ecr.ap-northeast-2.amazonaws.com/testserver:0.1.2 . --no-cache
-docker push 511624941057.dkr.ecr.ap-northeast-2.amazonaws.com/testserver:0.1.2
+docker build -t 511624941057.dkr.ecr.ap-northeast-2.amazonaws.com/testserver:0.2.0 . --no-cache
+docker push 511624941057.dkr.ecr.ap-northeast-2.amazonaws.com/testserver:0.2.0
