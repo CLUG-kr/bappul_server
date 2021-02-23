ff13f398c1f0b68bc1dbb023af4b53d6ac9e5c41
jaehoon0 <choco730522@gmail.com>
2021-02-23 20:34:35 +0900

    사이드바에 정보 띄워주기 위한 api 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,12 +2,15 @@ import { Controller, Get, Header, Post, Request, UseGuards } from '@nestjs/commo
 import { LocalAuthGuard } from './auth/local-auth.guard'
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { UserService } from './users/users.service'
 
 @Controller()
 
 @Controller()
 export class AppController {
-    constructor(private authService: AuthService) {}
+    constructor(
+        private authService: AuthService,
+        private userService: UserService) {}
 
     @Get()
     @Header('Cache-Control', 'no-store, no-cache')
@@ -27,5 +30,15 @@ export class AppController {
     getProfile(@Request() req) {
         console.log(req.user)
         return req.user;
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('profile/info')
+    async getNameAndEntranceYear(@Request() req) {
+        const user = await this.userService.findUserByCode(req.user.userId)
+        return {
+            "name": user.name,
+            "entranceYear": user.entranceYear,
+        }
     }
 }
